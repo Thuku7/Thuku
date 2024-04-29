4c8cc41420053794ba5ab419edd97c32f4aee4bc
Thuku7 <robethuku11@gmail.com>
2024-04-29 14:35:10 +0300

    Add the links to the stylesheet and script file and test the connection

---
+++ script/index.js
@@ -0,0 +1,8 @@
+let example = document.getElementById("test");
+
+example.addEventListener('click', exampleFunction);
+
+
+function exampleFunction( ) {
+  example.innerHTML = "Hello I'm working";
+}
