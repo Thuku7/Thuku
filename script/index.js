228e6593970c503c8bcf1085f52de2e54d7705b4
Thuku7 <robethuku11@gmail.com>
2024-05-11 18:39:46 +0300

    Add the typewriter functionality to the header

--- script/index.js
+++
@@ -1,8 +0,0 @@
-let example = document.getElementById("test");
-
-example.addEventListener('click', exampleFunction);
-
-
-function exampleFunction( ) {
-  example.innerHTML = "Hello I'm working";
-}
